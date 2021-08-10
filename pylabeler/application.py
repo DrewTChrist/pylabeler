362c907e00bb28514d7ecda2ca4d66fa1ba387d1
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2021-08-09 18:04:39 -0700

    Fixed #16. Added qt-material to requirements.txt and setup.py.

--- pylabeler/application.py
+++ pylabeler/application.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QApplication
+from qt_material import apply_stylesheet
 from pylabeler.ui.main_window import MainWindow
 import sys
 
@@ -7,6 +8,7 @@ class Application:
 
     def __init__(self):
         self._app = QApplication(sys.argv)
+        apply_stylesheet(self._app, 'dark_teal.xml')
         self._main_window = MainWindow()
         self._main_window.show()
         sys.exit(self._app.exec_())
--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
         'pyqtwebengine',
         'qtawesome',
         'blabel',
+        'qt-material'
     ],
     entry_points={
         'console_scripts': [
